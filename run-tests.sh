5f40fa95d926e6be938f6b097e7affea45574185
Kjetil Valle <kjetil.valle@gmail.com>
2014-01-19 14:07:15 +0100

    explicit ordering of tests in run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -4,9 +4,20 @@
 # Requires the python package `nose` to be installed, as well as the
 # `inotifytools` commands.
 
+function run_tests {
+    nosetests tests/test_1_parsing.py --stop
+    nosetests tests/test_2_evaluating_simple_expressions.py --stop
+    nosetests tests/test_3_evaluating_complex_expressions.py --stop
+    nosetests tests/test_4_working_with_variables_and_environments.py --stop
+    nosetests tests/test_5_adding_functions_to_the_mix.py --stop
+    nosetests tests/test_6_working_with_lists.py --stop
+    nosetests tests/test_7_final_language.py --stop
+
+    nosetests tests/test_builtins.py --stop
+    nosetests tests/test_provided_code.py --stop
+}
+
+run_tests
 while inotifywait -r -e modify . ; do
-    nosetests --stop
-    # nosetests tests/test_2_evaluating_simple_expressions.py --stop
-#    nosetests tests/test_provided_parsing.py --stop
-#    nosetests tests/test_1_parsing.py --stop
+    run_tests
 done
