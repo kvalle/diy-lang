b987aa2997faf5c9ac3a9fd7a9f69d728665cfe0
Kjetil Valle <kjetil.valle@gmail.com>
2014-03-22 15:33:21 +0100

    changed order of tests in run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,6 @@
 function run_tests {
     nosetests \
               tests/test_provided_code.py \
-              tests/test_sanity_checks.py \
               tests/test_1_parsing.py \
               tests/test_2_evaluating_simple_expressions.py \
               tests/test_3_evaluating_complex_expressions.py \
@@ -15,6 +14,7 @@ function run_tests {
               tests/test_5_adding_functions_to_the_mix.py \
               tests/test_6_working_with_lists.py \
               tests/test_7_using_the_language.py \
+              tests/test_sanity_checks.py \
               --stop
 }
 
